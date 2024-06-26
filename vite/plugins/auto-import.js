6d448177a22fee7f772c4f9a25a901d2141f0110
XFY9326 <xfy9326@qq.com>
2024-06-26 13:36:21 +0800

    Update dependencies and add more typing

--- vite/plugins/auto-import.js
+++ vite/plugins/auto-import.js
@@ -1,14 +1,17 @@
-import autoImport from "unplugin-auto-import/vite";
-import autoImportComponents from "unplugin-vue-components/vite";
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import path from "path";
 
 export default function createAutoImport() {
     return [
-        autoImport({
+        AutoImport({
             imports: ["vue", "vue-router", "pinia"],
+            resolvers: [ElementPlusResolver()],
             dts: path.resolve(process.cwd(), "./typing/auto-import.d.ts"),
         }),
-        autoImportComponents({
+        Components({
+            resolvers: [ElementPlusResolver()],
             dts: path.resolve(process.cwd(), "./typing/auto-import-components.d.ts"),
         }),
     ];
