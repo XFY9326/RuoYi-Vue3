db4c089af477dfbd1290e3eee722f4510315da1f
XFY9326 <xfy9326@qq.com>
2024-06-26 14:29:38 +0800

    Fix auto import

--- vite/plugins/auto-import.js
+++ vite/plugins/auto-import.js
@@ -1,17 +1,14 @@
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import path from "path";
 
 export default function createAutoImport() {
     return [
         AutoImport({
             imports: ["vue", "vue-router", "pinia"],
-            resolvers: [ElementPlusResolver()],
             dts: path.resolve(process.cwd(), "./typing/auto-import.d.ts"),
         }),
         Components({
-            resolvers: [ElementPlusResolver()],
             dts: path.resolve(process.cwd(), "./typing/auto-import-components.d.ts"),
         }),
     ];
