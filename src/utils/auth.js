966707c7bb01328d677122980d5db5bfdee17aed
XFY9326 <xfy9326@qq.com>
2024-10-19 14:05:51 +0800

    Add more types

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,6 @@
 import Cookies from "js-cookie";
 
-const TokenKey = "Admin-Token";
+const TokenKey = "token";
 
 /**
  * @returns {string}
---
+++ typing/ruoyi-request.d.ts
@@ -0,0 +1,15 @@
+// noinspection JSUnusedGlobalSymbols
+declare global {
+    interface AjaxResult<T = any> extends Record<String, any> {
+        code: number;
+        msg: string;
+        data: T?;
+    }
+
+    interface TableDataInfo<T = any> {
+        code: number;
+        msg: string;
+        total: number;
+        rows: Array<T>;
+    }
+}
